483968b0ad83c657634b35111d4224a87fc2d1dc
miwasaki <4324018@ed.tus.ac.jp>
2026-04-05 04:51:40 +0900

    testing a_maze_ing

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -2,9 +2,22 @@ from mazegen.MazeGenerator import MazeGenerator
 from mazegen.MazeSolver import MazeSolver
 from mazegen.Visualizer import Visualizer
 from mazegen.ConfigLoader import ConfigLoader
+from mazegen.MazeApp import MazeApp
+import sys
 
 
-def main():
+def main(argv: list[str]) -> None:
+    if len(argv) != 2:
+        print("Usage: python3 a_maze_ing.py <config.txt>")
+        return
+    try:
+        MazeApp().run(argv[-1])
+    except Exception as e:
+        print(e)
+    return
+
+
+def test() -> None:
     config = ConfigLoader()
     config = config.load(filepath="config.txt")
     maze = MazeGenerator.generate(config)
@@ -23,4 +36,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
+    # test()
