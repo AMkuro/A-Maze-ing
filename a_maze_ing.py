39312a71a4f0c1f78c175858da35794f04fb69f6
shu0219at <at398584@gmail.com>
2026-04-04 23:26:16 +0900

    modified:   a_maze_ing.py, pyproject.toml

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,1 +1,27 @@
+from mazegen.MazeGenerator import MazeGenerator
+from mazegen.MazeSolver import MazeSolver
+from mazegen.Visualizer import Visualizer
+from mazegen.ConfigLoader import ConfigLoader, AppConfig
 
+
+def main():
+    config = ConfigLoader()
+    config = config.load(filepath="config.txt")
+    maze = MazeGenerator.generate(config)
+    solution = MazeSolver.solve(maze)
+
+    print("Maze:")
+    visualize = Visualizer(maze, solution)
+    visualize.draw()
+
+    print("\nSolution path:")
+    for pos in solution.path:
+        print(pos)
+    visualize.toggle_path()
+    visualize.redraw()
+
+    print("\nNews string:")
+    print(solution.news)
+
+if __name__ == "__main__":
+    main()
