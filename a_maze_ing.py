810aa632c03586e233af92695cca29cde6d22a38
AMkuro <4324018@ed.tus.ac.jp>
2026-04-18 18:12:36 +0900

    delete test

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,7 +1,3 @@
-from mazegen.MazeGenerator import MazeGenerator
-from mazegen.MazeSolver import MazeSolver
-from mazegen.Visualizer import Visualizer
-from mazegen.ConfigLoader import ConfigLoader
 from mazegen.MazeApp import MazeApp
 import sys
 
@@ -17,24 +13,5 @@ def main(argv: list[str]) -> None:
     return
 
 
-def test() -> None:
-    config = ConfigLoader()
-    config = config.load(filepath="config.txt")
-    maze = MazeGenerator.generate(config)
-    solution = MazeSolver.solve(maze)
-
-    print("Maze:")
-    print([[str(i) for i in line] for line in maze.grid])
-    visualize = Visualizer(maze, solution)
-    visualize.draw()
-
-    print("\nSolution path:")
-    visualize.toggle_path()
-
-    print("\nNews string:")
-    print(solution.news)
-
-
 if __name__ == "__main__":
     main(sys.argv)
-    # test()
