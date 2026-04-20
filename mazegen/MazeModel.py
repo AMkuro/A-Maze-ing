61f69ad4ed97434086ade310dd19dc807c16dcf5
miwasaki <4324018@ed.tus.ac.jp>
2026-04-20 12:08:49 +0900

    import main

---
+++ mazegen/MazeModel.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+
+Grid = list[bytearray]
+Pos = tuple[int, int]
+
+
+class Wall:
+    NORTH = 1 << 0
+    EAST = 1 << 1
+    SOUTH = 1 << 2
+    WEST = 1 << 3
+    WALL_42 = 1 << 4
+    ALL_WALLS = NORTH | EAST | SOUTH | WEST
+
+
+@dataclass
+class Solution:
+    path: list[Pos]
+    news: str
+
+
+@dataclass
+class Maze:
+    width: int
+    height: int
+    grid: Grid
+    entry: Pos
+    exit: Pos
+    seed: int | None = None
