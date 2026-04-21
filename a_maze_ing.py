e2da853f37b5ad9c2f1c1a14dec8797ffbd7d0c3
miwasaki <4324018@ed.tus.ac.jp>
2026-04-21 12:29:47 +0900

    add error handling

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -2,16 +2,17 @@ from mazegen.MazeApp import MazeApp
 import sys
 
 
-def main(argv: list[str]) -> None:
+def main(argv: list[str]) -> int:
     if len(argv) != 2:
-        print("Usage: python3 a_maze_ing.py <config.txt>")
-        return
+        print("Usage: python3 a_maze_ing.py <config.txt>", file=sys.stderr)
+        return 2
     try:
         MazeApp().run(argv[-1])
     except Exception as e:
-        print(e)
-    return
+        print(e, file=sys.stderr)
+        return 1
+    return 0
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    sys.exit(main(sys.argv))
